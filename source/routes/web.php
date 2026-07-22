fe69a0b2d8c33c589bbb9f910bd21e0beb856144
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-07-22 21:14:16 +0200

    feat: start work session route

--- source/routes/web.php
+++ source/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\WorkSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::post('/work-sessions/start', [WorkSessionController::class, 'start'])
+        ->name('work-session.start');
 });
 
 require __DIR__.'/settings.php';
