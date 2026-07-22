c27bf03417bf25bd76c125c87f7618d8d1fcab98
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-07-22 18:42:41 +0200

    sync

--- source/app/Models/WorkBreak.php
+++ source/app/Models/WorkBreak.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['started_at', 'ended_at'])]
 class WorkBreak extends Model
 {
     public function workSession(): BelongsTo {
