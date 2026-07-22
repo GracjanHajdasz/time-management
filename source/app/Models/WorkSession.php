3e88859df5f304f4aebd76620b6b7d961f7ef582
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-07-22 18:27:30 +0200

    fix: add fillables to WorkSession model

--- source/app/Models/WorkSession.php
+++ source/app/Models/WorkSession.php
@@ -2,17 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[Fillable(['started_at', 'ended_at', 'note'])]
 class WorkSession extends Model
 {
-    public function user(): BelongsTo {
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function workBreaks(): HasMany {
+    public function workBreaks(): HasMany
+    {
         return $this->hasMany(WorkBreak::class);
     }
 }
