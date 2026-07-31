a14025244aa79faeba2d5a2952466a014247524c
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-07-31 13:16:05 +0200

    feat: add 'role' to users table

--- source/app/Models/User.php
+++ source/app/Models/User.php
@@ -29,7 +29,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
@@ -52,5 +52,15 @@ class User extends Authenticatable implements PasskeyUser
 
     public function workSessions(): HasMany {
         return $this->hasMany(WorkSession::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isEmployee(): bool
+    {
+        return $this->role === 'employee';
     }
 }
